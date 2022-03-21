d60a881aa04aae07487f6211f1d749e2ed4470f9
Tushar Tyagi <tushar.tyagi.dxb@gmail.com>
2022-03-20 23:03:48 -0700

    Create reverse_integer.py

---
+++ Arrays/reverse_integer.py
@@ -0,0 +1,10 @@
+#Reversing an integer's digits
+
+def reverse_integer(n):
+    rev = 0
+    rem = 0
+    while(n>0):
+        rem = n%10
+        rev = rev*10 + rem
+        n = n // 10
+    return rev
