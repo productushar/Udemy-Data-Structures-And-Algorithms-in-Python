f77fffcab48b54e22706a942baf5558fc568c143
Tushar Tyagi <tushar.tyagi.dxb@gmail.com>
2022-03-20 22:54:07 -0700

    Update reverse_array.py

--- Arrays/reverse_array.py
+++ Arrays/reverse_array.py
@@ -1,8 +1,6 @@
 #Reversing 1D Array of integers
 #Linear time complexity using constant memory
 
-x = [12,6,2,1,9,10,3]
-
 def reverse(nums):
     lowIndex = 0
     highIndex = len(nums)-1
@@ -11,5 +9,3 @@ def reverse(nums):
         lowIndex+=1
         highIndex-=1
     return nums
-
-print(reverse(x))
