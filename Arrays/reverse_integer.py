25cd74f8671df140f6a7f66ac96330b873f88a4f
Tushar Tyagi <tushar.tyagi.dxb@gmail.com>
2022-03-20 23:04:33 -0700

    Update reverse_integer.py

--- Arrays/reverse_integer.py
+++ Arrays/reverse_integer.py
@@ -3,8 +3,8 @@
 def reverse_integer(n):
     rev = 0
     rem = 0
-    while(n>0):
-        rem = n%10
-        rev = rev*10 + rem
+    while(n > 0):
+        rem = n % 10
+        rev = rev * 10 + rem
         n = n // 10
     return rev
